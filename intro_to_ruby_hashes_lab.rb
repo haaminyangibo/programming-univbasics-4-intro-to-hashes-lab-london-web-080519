e44d42e199cbbada27e0018399842687994f82da
Haami Nyangibo <haaminyangibo@gmail.com>
2019-08-02 19:01:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,8 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  read_from_hash = { :name => "Tzvi"}
+  
+  read_from_hash = { :name => 'Steve'}
   
   read_from_hash[:name]
 end
