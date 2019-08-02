9764aacf9df16cfd3221ca7ac1731b53e611399d
Haami Nyangibo <haaminyangibo@gmail.com>
2019-08-02 19:13:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,6 +21,7 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
+ 
   key = :name
   
   my_hash_creator = { :name => "Grace Hopper"}
